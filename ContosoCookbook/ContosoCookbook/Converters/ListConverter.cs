147fd18786d73ff335d6215be3ed2f16b0eb74b8
Van Tian <vantian@live.com>
2017-12-12 23:42:32 +0800

    功能：完成菜谱页面

---
+++ ContosoCookbook/ContosoCookbook/Converters/ListConverter.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ContosoCookbook.Converters
+{
+    public class ListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Join("\r\n", (IEnumerable<string>)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ContosoCookbook/ContosoCookbook/ViewModels/RecipePageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ContosoCookbook.Models;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoCookbook.ViewModels
+{
+    public class RecipePageViewModel : ViewModelBase, INavigatingAware
+    {
+        private Recipe _recipe;
+        public Recipe Recipe
+        {
+            get => _recipe;
+            set => SetProperty(ref _recipe, value);
+        }
+
+        public override void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            base.OnNavigatedFrom(parameters);
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("recipe"))
+                Recipe = (Recipe)parameters["recipe"];
+        }
+    }
+}
