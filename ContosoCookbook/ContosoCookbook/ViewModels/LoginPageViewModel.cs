249c077a650d98f2ff931f4c93f410a42cf71f56
Van Tian <vantian@live.com>
2017-12-14 04:41:42 +0800

    功能：增加LoginPageViewModel

---
+++ ContosoCookbook/ContosoCookbook/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoCookbook.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+        private string _userName;
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+    }
+}
